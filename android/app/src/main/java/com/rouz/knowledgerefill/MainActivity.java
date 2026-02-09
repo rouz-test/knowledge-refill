e6e1f085cdaae46ffe1ca72f22632484a5bc7f44
rouz <rouz@mail.com>
2026-02-09 16:52:45 +0900

    안드로이드 알림 아이콘 (로컬)

--- android/app/src/main/java/com/rouz/knowledgerefill/MainActivity.java
+++ android/app/src/main/java/com/rouz/knowledgerefill/MainActivity.java
@@ -1,5 +1,16 @@
 package com.rouz.knowledgerefill;
 
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    // Force dark mode regardless of system setting
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+    super.onCreate(savedInstanceState);
+  }
+}
